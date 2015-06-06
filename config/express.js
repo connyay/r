67ea209d800f61f03b88eaf4a1ab1dfb952371d8
Connor Hindley <connor@redhat.com>
2015-06-06 17:24:06 -0400

    add cors because I'm lazy

--- config/express.js
+++ config/express.js
@@ -1,9 +1,11 @@
 'use strict';
 
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 module.exports = function (app) {
     app.use(bodyParser.json());
+    app.use(cors());
 
     app.get('/', function (req, res) {
         res.end('lub-dub');
