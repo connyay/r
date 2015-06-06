14d67fbf3a3d19e76366061a572e21b57c7b41f7
Spenser Shumaker <sshumake@redhat.com>
2015-06-06 18:44:46 -0400

    tags

--- config/seed/relations.js
+++ config/seed/relations.js
@@ -2,12 +2,18 @@
 var Q = require('q');
 
 module.exports = function (tags, types, recommendations) {
+	var maxNumberOfTags = 4
     var dfd = Q.defer();
     var dfdList = [];
     recommendations.forEach(function (r) {
-        dfdList.push(r.addTag(tags[0], {
-            score: 90
-        }));
+    	var numTags = Math.floor(Math.random() * maxNumberOfTags);
+    	for(var i = 0; i < numTags; i++){
+    		var tagNum = Math.floor(Math.random() * tags.length);
+    		var score = Math.floor(Math.random() * 100);
+    		dfdList.push(r.addTag(tags[tagNum], {	
+            	score: score
+        	}));
+    	}
     });
     Q.all(dfdList).then(dfd.resolve);
     return dfd.promise;
